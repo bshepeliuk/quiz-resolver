2d05a4fb8b5190e556a2ab12f721d75f89660106
bshepeliuk <bogdan__sh@ukr.net>
2023-04-05 17:32:50 +0300

    first commit

---
+++ server/src/index.ts
@@ -0,0 +1,32 @@
+import fastify from 'fastify';
+
+const envToLogger = {
+  development: {
+    transport: {
+      target: 'pino-pretty',
+      options: {
+        translateTime: 'HH:MM:ss Z',
+        ignore: 'pid,hostname',
+      },
+    },
+  },
+  production: true,
+  test: false,
+};
+
+const app = fastify({ logger: envToLogger.development });
+
+app.get('/', (_, reply) => {
+  reply.status(200).send({ hello: 'world' });
+});
+
+const start = async () => {
+  try {
+    await app.listen({ port: 3000 });
+  } catch (err) {
+    app.log.error(err);
+    process.exit(1);
+  }
+};
+
+start();
